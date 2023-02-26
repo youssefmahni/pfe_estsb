b03b895f3de58d9bbfd6febfde625f93f4826736
youssefmahni <youssefmahni11@gmail.com>
2023-02-26 14:01:20 +0100

    react tools

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-if (ProcessingInstruction.env.NODE_ENV !== 'production') disableReactDevTools()
+// if (ProcessingInstruction.env.NODE_ENV !== 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
